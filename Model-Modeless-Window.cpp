0bab7a4454e2f4b54182a5347be2b15c87e49a44
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-06 12:09:16 +0530

    Create Model-Modeless-Window.cpp

---
+++ Model-Modeless-Window.cpp
@@ -0,0 +1,7 @@
+The difference between modal and modeless dialogs is not limited to MFC.
+
+When a modal dialog is open you cannot interact with anything else than this modal 
+dialog inside your program, as long as the modal dialog is open. Most dialogs are modal, 
+for example the File-Save As dialogs are modal.On the other hand a modeless dialog behaves
+just like a normal window, you can do anything you want while it is open. The spell checker
+dialog in Microsoft Word is an example of such a dialog.
