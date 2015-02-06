bd1f34a2a8d0a4ef933419833a50f1a8902e02c2
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-06 11:55:18 +0530

    Update Get-Peek-Message.cpp

--- Get-Peek-Message.cpp
+++ Get-Peek-Message.cpp
@@ -5,3 +5,44 @@ message to retrieve from the Message Queue and PeekMessage() returns immediately
 weather there are any messages or not.While PostMessage is used to post a message
 to window and it returns immediately without waiting for the response. 
 
+GetMessage:
+
+a. It retrieves the message from the calling thread's message queue.
+b. It blocks until it gets a message or an error occurs.
+c. It can filter message as per specified filter.
+
+Example:
+
+Code:
+BOOL bRet;
+MSG msg;
+
+while( (bRet = GetMessage( &msg, hWnd, 0, 0 )) != 0)
+{ 
+    if (bRet == -1)
+    {
+        // handle the error and possibly exit
+    }
+    else
+    {
+        TranslateMessage(&msg); 
+        DispatchMessage(&msg); 
+    }
+}
+PeekMessage:
+a. It checks for the message in the calling thread's message queue and it 
+retrieves the message if any present.
+b. As specified in the argument, it may or may not removes the message from message queue.
+c. It returns immediately if there is no message found in the message queue. 
+Hence it can be useful in some situation to process message in between lengthy operation.
+
+Example:
+
+Code:
+HWND hwnd; 
+MSG msg; 
+ 
+while (PeekMessage(&msg, hwnd,  0, 0, PM_REMOVE)) 
+{ 
+     //handle message here...
+}
